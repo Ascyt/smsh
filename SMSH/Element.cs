8d993479c1b17e062c073759f76d3b76d43e3b7a
Filip Schauer <f.schauer@students.htl-leonding.ac.at>
2023-10-08 12:39:04 +0200

    Extracted code from Elements into seperate files

---
+++ SMSH/Element.cs
@@ -0,0 +1,77 @@
+﻿using static Program;
+
+namespace Elements
+{
+    public partial class Elements
+    {
+        public class Element
+        {
+            public string tag;
+            public readonly Dictionary<string, string> attributes = new Dictionary<string, string>();
+            
+            public string text;
+            public List<Element> elements = new List<Element>();
+
+            public Element(string tag, string text = "", Dictionary<string, string>? attributes = null)
+            {
+                if (tag.Length > 0 && (tag[0] == '.' || tag[0] == ':'))
+                    tag = tag.Substring(1);
+
+                this.attributes = attributes ?? new Dictionary<string, string>();
+
+                if (new string[] { "card" }.Contains(tag))
+                {
+                    if (this.attributes.ContainsKey("class"))
+                        this.attributes["class"] += " " + tag;
+                    else
+                        this.attributes["class"] = tag;
+                    this.tag = "div";
+                }
+                else
+                {
+                    if (tag == "")
+                    {
+                        if (this.attributes.ContainsKey("class"))
+                            this.attributes["class"] = "t " + this.attributes["class"];
+                        else
+                            this.attributes["class"] = "t";
+
+                        this.tag = "span";
+                    }
+                    else
+                        this.tag = tag;
+                }
+
+                this.text = text;
+            }
+           
+            public override string ToString()
+            {
+                string result = $"<{tag}";
+
+                // attributes
+                foreach (KeyValuePair<string, string> attribute in attributes)
+                {
+                    if (attribute.Value == null)
+                        continue;
+
+                    result += $" {attribute.Key}=\"{attribute.Value}\"";
+
+                    if (attribute.Key == "href")
+                        result += " target=\"_blank\"";
+                }
+                result += ">";
+
+                // body
+                if (text != "")
+                    result += text;
+                foreach (Element element in elements)
+                    result += element.ToString();
+                result += $"</{tag}>";
+
+                return result;
+            }
+        }
+    
+    }
+}
---
+++ SMSH/FormatText.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Program;
+
+namespace Elements  
+{
+    public partial class Elements
+    {
+        public static string FormatText(string text, int indents, int lineIndex)
+        {
+            text = text.Replace("\\\\", "&#92;").Replace("\\<", "&lt;").Replace("\\>", "&gt;");
+
+            string result = "";
+            string currentFormat = "";
+            int formattingDelta = 0;
+            const char FORMAT_START = '<';
+            const char FORMAT_END = '>';
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == FORMAT_START)
+                {
+                    formattingDelta++;
+
+                    if (formattingDelta == 1)
+                        continue;
+                }
+
+                if (text[i] == FORMAT_END)
+                {
+                    formattingDelta--;
+
+                    if (formattingDelta < 0)
+                        throw new CodeException($"Unexpected format closing tag.", lineIndex, indents, i);
+
+                    if (formattingDelta == 0)
+                    {
+                        if (new string[] { "br", "hr", "img", "input", "meta", "link" }.Contains(currentFormat.Trim()))
+                        {
+                            result += $"<{currentFormat}>";
+                        }
+                        else
+                        {
+                            Dictionary<string,string> attributes = ExtractAttributes(lineIndex, ref currentFormat, getAttributes);
+
+                            string formatTag = currentFormat.Split(' ')[0];
+
+                            currentFormat = FormatText(currentFormat.Substring(formatTag.Length).TrimStart(), indents, lineIndex);
+
+                            Element element = new Element(formatTag, currentFormat, attributes);
+                            result += element.ToString();
+                        }
+
+                        currentFormat = "";
+
+                        continue;
+                    }
+                }
+
+                if (formattingDelta > 0)
+                {
+                    currentFormat += text[i];
+                    continue;
+                }
+
+                result += text[i];
+            }
+
+            if (formattingDelta > 0)
+                throw new CodeException("Unclosed format tag.", lineIndex, indents, text.Length);
+
+            return result;
+        }
+
+
+    }
+}
